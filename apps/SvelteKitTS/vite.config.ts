cc08997a8017016ee65098f8ba860c18c4d93cfc
ArMar <mr.arthur.marchand@gmail.com>
2023-11-22 21:32:44 +0100

    u

--- apps/SvelteKitTS/vite.config.ts
+++ apps/SvelteKitTS/vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
       check: false,
 
       tsconfig: "tsconfig.json", // Is the default, would throw an error if tsconfig was not found
+      // No effect
+      // include: ["*.ts+(|x)", "**/*.ts+(|x)", "**/*.cts", "**/*.mts"],
+      // exclude: ["**/node_modules/**/*"],
 
       // No effect
       // noEmit: true,
